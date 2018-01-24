80e8af71627c20a48c8c9e01e309e7b354f36b78
matthiasbuhl <35766446+matthiasbuhl@users.noreply.github.com>
2018-01-24 14:37:01 +0100

    Add files via upload

---
+++ master_readwrite/master_readwrite.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+
+
+#define PAYLOAD_SIZE 2
+#define NEW_NODE_ADDRESS 14
+byte NODE_ADDRESS = 8;
+
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  delay(3000);
+}
+
+void loop() {
+  Wire.requestFrom(NODE_ADDRESS, 1, false);
+  byte RANDOM_NUMBER  = Wire.read(); // receive a byte as characte
+  //Wire.endTransmission();
+  Serial.println(RANDOM_NUMBER);         // print the character
+
+  byte buffer[PAYLOAD_SIZE]; // Load buffer 
+  buffer[0] = RANDOM_NUMBER;
+  buffer[1] = NEW_NODE_ADDRESS;
+  
+  Wire.beginTransmission(NODE_ADDRESS);
+  Wire.write(buffer, PAYLOAD_SIZE);
+  Wire.endTransmission();
+
+  
+  delay(2000);
+}
---
+++ slave1_writeread/slave1_writeread.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+
+#define RANDOM_NUMBER 220
+
+bool LED_STATE = false;
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  for (int i=0; i<=3; i++) {
+  digitalWrite(LED1, HIGH);
+  delay(200);
+  digitalWrite(LED1, LOW);
+  delay(200);
+  }
+}
+
+void loop() {
+  delay(100);
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  Wire.write(RANDOM_NUMBER);
+}
+
+void receiveEvent(int howMany) {
+    if (howMany == 2){
+    byte x = Wire.read();
+    Serial.println(x);
+    byte NEW_NODE_ADDRESS = Wire.read();
+    Serial.println(NEW_NODE_ADDRESS);
+      if ((x == RANDOM_NUMBER) && (LED_STATE == false)) {
+        digitalWrite(LED1, HIGH);
+        LED_STATE = true;
+        Wire.begin(NEW_NODE_ADDRESS);
+      }
+      else {
+        digitalWrite(LED1, LOW);
+        LED_STATE = false;
+      } 
+    }
+}
