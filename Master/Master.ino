5c6588871d83286dc708f1d9600e07b65c072be2
matthiasbuhl <35766446+matthiasbuhl@users.noreply.github.com>
2018-02-21 15:06:13 +0100

    Add files via upload

--- Master/Master.ino
+++ Master/Master.ino
@@ -3,6 +3,7 @@
 
 bool DEVICE_RESPONSE = true;
 bool DUPLICATE = false;
+bool NEW_DEVICE_DETECTED = false;
 byte READ_PAYLOAD_SIZE = 2;
 byte WRITE_PAYLOAD_SIZE = 3;
 byte NODE_ADDRESS = 9;
@@ -17,9 +18,8 @@ int const ARRAY_LENGTH = 112;
 uint16_t RANDOM_NUMBER;
 uint16_t DUPLICATE_NUMBER;
 
-uint16_t fake = 49;
-
 uint16_t NUM_MEMORY[ARRAY_LENGTH];
+int ADDRESS_MEMORY[ARRAY_LENGTH];
 
 
 void setup() {
@@ -32,12 +32,10 @@ void setup() {
 
 void loop() {
   Serial.println("workin");
-  
-  print_memory();
   resolve_duplicate();
+  delay(500);
   enumeration();
-  
-
+  print_memory();
   delay(2000);
 }
 
@@ -58,18 +56,18 @@ void receiveEvent(int howMany) {
         NUM_MEMORY[i] = RANDOM_NUMBER;
         Serial.println(RANDOM_NUMBER);
         i++;
+        NEW_DEVICE_DETECTED = true;
       }
       else if (error == 1) {
         DUPLICATE_NUMBER = RANDOM_NUMBER;
         Serial.println("Duplicate detected!");
+        i--;
         DUPLICATE = true;
       }
 
       else if (i >= 112) {
         Serial.println("All addresses already in use!");
-      }
-
-      
+      } 
     }
   }
 }
@@ -89,37 +87,47 @@ int search_duplicates(int arr[], const int value, const int step) {
   else {
     return 0;
   }
+  Serial.println("No duplicates detected");
 }
 
 
 void enumeration() {
-  for (k = 0; k < i; k++) {
+  if (NEW_DEVICE_DETECTED == true) {
   byte error;
   Wire.beginTransmission(NODE_ADDRESS);
   error = Wire.endTransmission();
   
 
-  if ((error == 0) && (NEW_NODE_ADDRESS < 120)) {
-    byte buffer[WRITE_PAYLOAD_SIZE]; 
-    buffer[0] = (NUM_MEMORY[k] >> 8) & 0xFF;
-    buffer[1] = NUM_MEMORY[k] & 0xFF;
-    buffer[2] = NEW_NODE_ADDRESS;
-    
-    Wire.beginTransmission(NODE_ADDRESS);
-    Wire.write(buffer, WRITE_PAYLOAD_SIZE);
-    Wire.endTransmission();
-    NEW_NODE_ADDRESS++;
-    Serial.print("New address assigned to ");
-    Serial.println(NUM_MEMORY[k]);
-    delay(500);
+    if ((error == 0) && (NEW_NODE_ADDRESS < 120)) {
+      for (k = k; k < i; k++) {
+      byte buffer[WRITE_PAYLOAD_SIZE]; 
+      buffer[0] = (NUM_MEMORY[k] >> 8) & 0xFF;
+      buffer[1] = NUM_MEMORY[k] & 0xFF;
+      buffer[2] = NEW_NODE_ADDRESS;
+      ADDRESS_MEMORY[k] = NEW_NODE_ADDRESS;
+      Wire.beginTransmission(NODE_ADDRESS);
+      Wire.write(buffer, WRITE_PAYLOAD_SIZE);
+      Wire.endTransmission();
+      NEW_NODE_ADDRESS++;
+      Serial.print("New address assigned to ");
+      Serial.println(NUM_MEMORY[k]);
+      NUM_MEMORY[k] = 0;
+      delay(500);
+      }
+    }
+    else if (error == 2) {
+      Serial.println("All devices on bus assigned");
+    }
+    else if (error == 4) {
+      Serial.println("All addresses already in use!");
+    }
+    else {
+      return;
+    }
+   NEW_DEVICE_DETECTED = false; 
   }
-  else if (error == 2) {
-    Serial.println("All devices on bus assigned");
-  }
-  else if (error == 4) {
-    Serial.println("All addresses already in use!");
-  }
-
+  else {
+    return;
   }
 }
 
@@ -142,8 +150,11 @@ void resolve_duplicate() {
 
 
 void print_memory() {
-  for (int k = 0; k < i; k++) {
-    Serial.println(NUM_MEMORY[k]);
-  }
+    for (int k = 0; k < i; k++) {
+      String Str1 = "Device";
+      String Str2 = " connected at: 0x";
+      Serial.print(Str1 + (k+1) + Str2);
+      Serial.println(ADDRESS_MEMORY[k]);
+    }
 }
 
